1b8394d032a07754f8a040d22af3815ea5ea3653
Victor Almeida <jpvvictor@hotmail.com>
2025-05-19 17:30:05 -0300

    chore: adding sass dependency and adjust scss files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,20 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/test-utils']
+  css: ['~/assets/scss/main.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/scss/utils/_variables.scss" as *; @use "~/assets/scss/utils/_mixins.scss" as *;'
+        }
+      }
+    }
+  },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/test-utils',
+    '@nuxt/icon'
+  ]
 })
