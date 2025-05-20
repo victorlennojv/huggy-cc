cf2cd3bdefc1940c5b4fd9695a29e07f8e44da99
Victor Almeida <jpvvictor@hotmail.com>
2025-05-20 14:39:57 -0300

    feat(component): add new component BaseCard

---
+++ components/common/BaseCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="base-card">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+.base-card {
+  background: var(--fill-neutral-low-0);
+  border-radius: 16px;
+  box-shadow: var(--shadow-md);
+  padding: 32px 24px;
+  width: 100%;
+}
+</style> 
